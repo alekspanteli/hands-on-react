fb2225cf46e5e905fd7acdfbf1e8e4cfb3a3b3de
alekspanteli <alekspanteli@rambler.ru>
2022-10-26 09:20:40 +0000

    Update App.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 // import logo from "./logo.png";
 
-// function Image() {
-//   return <img src="./logo.png" alt="test" />;
-// }
-
 function App() {
   // const logo = <img src="./logo.png" alt="test" />;
-  const logo = {
-    name: "React Logo",
-    title: "React Logo",
-    path: "./logo.svg",
-  };
+  // const logo = {
+  //   name: "React Logo",
+  //   title: "React Logo",
+  //   path: "./logo.svg",
+  // };
+  const list = (
+    <>
+      <div className="post">
+        <h1>My First Blog Post</h1>
+        <div>Author: Mark Twain</div>
+        <div>Published: {new Date().toLocaleTimeString()}</div>
+        <p>
+          I am new to blogging and this is my first post. You should expect a
+          lot of great things from me.
+        </p>
+      </div>
+    </>
+  );
   return (
     <div className="container mx-auto px-4">
       {/* {logo} */}
       {/* <Image /> */}
-      <img src={logo.path} alt={logo.name} title={logo.title} />
+      {/* <img src={logo.path} alt={logo.name} title={logo.title} /> */}
+      {list}
     </div>
   );
 }
