86a803f0a1bbcac0d689a361451f8743f3a40b3a
alekspanteli <alekspanteli@rambler.ru>
2022-10-26 07:26:28 +0000

    Update 22 files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const logo = <img src="./react-logo.png" />;
+  return (
+    <div className="container mx-auto px-4">
+      <header>test</header>
+    </div>
+  );
+}
+
+export default App;
