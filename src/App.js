94f0be9ff93bea099c1d42395fa00d5a569b7a16
alekspanteli <alekspanteli@rambler.ru>
2022-10-26 09:30:06 +0000

    Update 2 files

--- src/App.js
+++ src/App.js
@@ -1,31 +1,19 @@
 // import logo from "./logo.png";
+import "./Button.css";
+
+function HelloWorld() {
+  return <div className="container">Hello Function Component</div>;
+}
+
+function Button() {
+  return <button className="btn">Click me</button>;
+}
 
 function App() {
-  // const logo = <img src="./logo.png" alt="test" />;
-  // const logo = {
-  //   name: "React Logo",
-  //   title: "React Logo",
-  //   path: "./logo.svg",
-  // };
-  const list = (
-    <>
-      <div className="post">
-        <h1>My First Blog Post</h1>
-        <div>Author: Mark Twain</div>
-        <div>Published: {new Date().toLocaleTimeString()}</div>
-        <p>
-          I am new to blogging and this is my first post. You should expect a
-          lot of great things from me.
-        </p>
-      </div>
-    </>
-  );
   return (
     <div className="container mx-auto px-4">
-      {/* {logo} */}
-      {/* <Image /> */}
-      {/* <img src={logo.path} alt={logo.name} title={logo.title} /> */}
-      {list}
+      <HelloWorld />
+      <Button />
     </div>
   );
 }
