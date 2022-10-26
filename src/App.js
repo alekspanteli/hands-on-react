690c39798a8016e3591d7bb67ba8a243148062fc
alekspanteli <alekspanteli@rambler.ru>
2022-10-26 11:48:39 +0000

    Update 4 files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 // import logo from "./logo.png";
 // import "./Button.css";
-import ProjectsPage from "./projects/ProjectsPage";
+// import ProjectsPage from "./projects/ProjectsPage";
+
+// function Greeter(props) {
+//   const { first, last } = props;
+//   return <h1>Hello, {`${first} ${last}`}</h1>;
+// }
+
+function Greeter({ first, last }) {
+  return <h1>Hello, {`${first} ${last}`}</h1>;
+}
 
 function App() {
+  const Person = {
+    firstName: "Alex",
+    lastName: "Panteli",
+  };
   return (
     <div className="container mx-auto px-4">
-      <ProjectsPage />
+      <Greeter first={Person.firstName} last={Person.lastName} />
     </div>
   );
 }
